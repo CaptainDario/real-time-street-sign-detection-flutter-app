234bd9ac5aa8aa981c6caf048e0b293fd7888021
CaptainDario @ MBP M1 <daapplab@gmail.com>
2022-12-14 17:50:31 +0100

    renamed

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Object Detection TFLite',
+      title: 'DCAITI - Street Sign Detection',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
