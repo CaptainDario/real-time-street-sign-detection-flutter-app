d7d5d0a22dfc6239cfd89d8f11d78a35b7be7dc5
CaptainDario @ MBP M1 <daapplab@gmail.com>
2022-12-17 14:49:31 +0100

    tu + dcaiti logo

--- lib/ui/info_page.dart
+++ lib/ui/info_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:street_sign_detection/theme.dart';
 
 
@@ -20,9 +21,28 @@ class InfoPage extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.all(8),
-        child: Text(
-          "\n\nThis project is a research coorporation between DCAITI and the TU Berlin.\n"
-          "Developers: Dario Klepoch, Marvin Beese, Clemens Lottermoser",
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.width*0.2,
+                  child: Image.asset("assets/icon/tu.png"),
+                ),
+                Container(
+                  color: Colors.green,
+                  height: MediaQuery.of(context).size.width*0.2,
+                  child: SvgPicture.asset("assets/icon/dcaiti.svg"),
+                  //color: Colors.white,
+                )
+              ],
+            ),
+            Text(
+              "\n\nThis project is a research coorporation between DCAITI and the TU Berlin.\n"
+              "Developers: Dario Klepoch, Marvin Beese, Clemens Lottermoser",
+            ),
+          ],
         ),
       ),
     );
