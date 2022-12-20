b3f1d0b2b5471f26f922d79c833d17b7ff26c001
CaptainDario @ MBP M1 <daapplab@gmail.com>
2022-12-20 12:06:23 +0100

    get platform dependent available backends

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,11 +1,34 @@
+import 'package:flutter/material.dart';
 
-
-
-
+import 'package:street_sign_detection/ml_models.dart';
 import 'package:street_sign_detection/tflite/inference_backend.dart';
+import 'package:universal_io/io.dart';
 
-class Settings {
 
-  InferenceBackend? inferenceBackend = InferenceBackend.CPU;
 
+/// Class to bundle all settings
+class Settings with ChangeNotifier{
+
+  /// the currently selected inference backend
+  InferenceBackend inferenceBackend = InferenceBackend.CPU;
+  /// The currently selected ml model
+  MLModels mlModel = MLModels.YOLOV5;
+
+  List<InferenceBackend> inferenceBackends = [InferenceBackend.CPU];
+
+  List<InferenceBackend> inferenceBackendsAndroid = [
+    InferenceBackend.NNApi, InferenceBackend.GPU, InferenceBackend.XNNPack
+  ];
+
+  List<InferenceBackend> inferenceBackendsIos = [
+    InferenceBackend.CoreML, InferenceBackend.Metal, InferenceBackend.XNNPack
+  ];
+
+
+  Settings(){
+    if(Platform.isAndroid)
+      inferenceBackends.addAll(inferenceBackendsAndroid);
+    else if(Platform.isIOS)
+      inferenceBackends.addAll(inferenceBackendsIos);
+  }
 }
