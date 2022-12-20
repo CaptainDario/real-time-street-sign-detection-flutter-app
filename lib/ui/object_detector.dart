aae2482c4f4f5190daa4ea0da6ef713ac6dc1428
CaptainDario @ MBP M1 <daapplab@gmail.com>
2022-12-20 12:24:41 +0100

    moved draggable sheet out of object detector class

--- lib/ui/object_detector.dart
+++ lib/ui/object_detector.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:camera/camera.dart';
 import 'package:provider/provider.dart';
+
 import 'package:street_sign_detection/tflite/object_detection.dart';
 
 import 'object_detection_painter.dart';
@@ -11,7 +12,9 @@ import 'package:street_sign_detection/ui/live_camera_preview.dart';
 
 
 
+/// A widget to show a live camera feed and draw object detections on top of it
 class ObjectDetector extends StatefulWidget {
+
   const ObjectDetector({super.key});
 
   @override
@@ -19,6 +22,7 @@ class ObjectDetector extends StatefulWidget {
 }
 
 class _ObjectDetectorState extends State<ObjectDetector> {
+
 
   @override
   void initState() {
@@ -36,51 +40,14 @@ class _ObjectDetectorState extends State<ObjectDetector> {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
       value: GetIt.I<ObjectDetections>(),
-      builder: (context, child) => Stack(
-        children: <Widget>[
-          
-          SizedBox(
-            child: LiveCameraPreview(
-              cameraDescription: GetIt.I<List<CameraDescription>>()[0],
-            ),
-          ),
-          Container(
-            height: 240,
-            width: 320,
-            child: CustomPaint(painter: ObjectDetectionPainter(
-              context.watch<ObjectDetections>().objectDetections
-            ))
-          ),
-          
-          /*
-          // change camera button
-          Positioned(
-            bottom: 50,
-            right: 50,
-            child: Container(
-              height: 100,
-              child: IconButton(
-                icon: Icon(Icons.cameraswitch),
-                onPressed: () {},
-              ),
-            ),
-          ),
-          */
-    
-          // Bottom Sheet
-          /*
-          Positioned(
-            bottom: 0,
-            width: MediaQuery.of(context).size.width,
-            child: BottomInfoSheet(
-              mlModel: mlModel,
-              onChanged: (value) => setState(() {
-                mlModel = value;
-              }),
-            )
+      builder: (context, child) => LiveCameraPreview(
+        cameraDescription: GetIt.I<List<CameraDescription>>()[0],
+        child: CustomPaint(
+          painter: ObjectDetectionPainter(
+            context.watch<ObjectDetections>(),
+            Size(720, 1280)
           )
-          */
-        ],
+        ),
       ),
     );
   }
