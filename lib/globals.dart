c98a82c7887e254901029b86ea246fc70a80810f
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-02-02 00:10:17 +0100

    global configuration values

---
+++ lib/globals.dart
@@ -0,0 +1,17 @@
+
+
+int cameraImageHeight   = 1280;
+int cameraImageWidth    = 720;
+int cameraImageChannels = 3;
+
+int yolo5InWidth    = 300;
+int yolo5InHeight   = 300;
+int yolo5InChannels = 3;
+
+int yolo7InWidth    = 300;
+int yolo7InHeight   = 300;
+int yolo7InChannels = 3;
+
+int currentMlModelInHeight   = yolo5InHeight;
+int currentMlModelInWIdth    = yolo5InWidth;
+int currentMlModelInChannels = yolo5InChannels;
